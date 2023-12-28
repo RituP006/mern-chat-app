65f9b88d85c7aa6ca0d1787d8b631fb26fea4f07
Ritu Patel <ritupatel660@gmail.com>
2023-12-28 19:40:12 +0530

    production changes

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
 dotenv.config();
 connectDB();
@@ -15,13 +16,27 @@ const app = express();
 
 app.use(express.json()); //to accept json data
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
-});
-
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
 
 // Error Handling middlewares
 app.use(notFound);
